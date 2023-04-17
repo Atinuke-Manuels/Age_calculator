9ac466f8c23992e741216dafb4f4ef5f7e159dc6
Atinuke <aodeshola@yahoo.com>
2023-04-17 14:58:55 +0100

    Save

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+   
+    const years =document.getElementById('years').innerHTML;
+    const months = document.getElementById('months').innerHTML;
+    const days = document.getElementById('days').innerHTML;
+
+    const day_error = document.getElementById("day_error").innerHTML;
+    const month_error = document.getElementById("month_error").innerHTML;
+    const year_error =  document.getElementById("year_error").innerHTML;
+
+
+
 function age() {
-    let d1 = document.getElementById('dayInput').value;
-    let m1 = document.getElementById('monthInput').value;
-    let y1 = document.getElementById('yearInput').value;
+    const d1 = document.getElementById('dayInput').value;
+    const m1 = document.getElementById('monthInput').value;
+    const y1 = document.getElementById('yearInput').value;
 
     let date = new Date();
     let d2 = date.getDate();
@@ -26,8 +37,58 @@ function age() {
     let m = m2 - m1;
     let y = y2 - y1;
 
-    document.getElementById('years').innerHTML = +y+ " years";
-    document.getElementById('months').innerHTML = +m+ " months";
-    document.getElementById('days').innerHTML = +d+ " days"
+    if(d1 < 0 || d1 > 31 ){
+        document.getElementById("day_error").innerHTML= "incorrect format";
+        document.getElementById("day_error").style.color='red';
+        document.getElementById("dayInput").style.color='red';
+        // document.getElementById("dayInput").style.borderColor='red';
+
+        //Helps to ensure incorrect information is not submitted.
+        return false;
+    }
+
+        if(m1 < 0 || m1 > 12 ){
+        document.getElementById("month_error").innerHTML= "incorrect format";
+        document.getElementById("month_error").style.color='red';
+        document.getElementById("monthInput").style.color='red';
+        // document.getElementById("monthInput").style.borderColor='red';
+
+        //Helps to ensure incorrect information is not submitted.
+        return false;
+    }
+
+        if( y1 > date.getFullYear()){
+        document.getElementById("year_error").innerHTML= "incorrect format";
+        document.getElementById("year_error").style.color='red';
+        document.getElementById("yearInput").style.color='red';
+        // document.getElementById("yearInput").style.borderColor='red';
+        //Helps to ensure incorrect information is not submitted.
+        return false;
+    }
+
+    document.getElementById('years').innerHTML = +y+ ' ';
+    document.getElementById('months').innerHTML = +m+ ' ';
+    document.getElementById('days').innerHTML = +d+ ' ';
+
 }
+
+function reset() {
+  // Reset input fields
+  document.getElementById("dayInput").value = "";
+  document.getElementById("monthInput").value = "";
+  document.getElementById("yearInput").value = "";
+  
+  // Reset output values
+  document.getElementById("years").innerHTML = "--";
+  document.getElementById("months").innerHTML = "--";
+  document.getElementById("days").innerHTML = "--";
+  
+  // Clear error messages
+  document.getElementById("day_error").innerHTML = "";
+  document.getElementById("month_error").innerHTML = "";
+  document.getElementById("year_error").innerHTML= "";
+}
+
+
+
 
